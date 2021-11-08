4fbaecb078f31484d75f5fffbc0e5e311d1a13ab
Gra4git <Gra4git@gmail.com>
2021-11-08 14:14:52 +0300

    release4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from "react-dom";
 import React from 'react'
+import img1 from "../assets/r1.png"
+import "./styles.css"
 
-ReactDOM.render(<h1>Hello, intocode</h1>, document.getElementById("root"));
+ReactDOM.render(<>
+<h1>Hello, intocode</h1>
+<img className="logo" src= {img1}/></>, document.getElementById("root"));
 
